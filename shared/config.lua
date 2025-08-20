76a0c77f64fbfb7cd615d2d5fc30bf45ddd98b30
shreddykr <148612874+shreddykr@users.noreply.github.com>
2025-08-20 15:36:24 -0700

    config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -1,20 +1,20 @@
 Config = {}
 
 Config.VapeItem = "vape"
-Config.MaxHits = 200
+Config.MaxHits = 50
 
 Config.RefillItems = {
-    ["vape_refill_strawberry"] = { amount = 50, uses = 8 },
-    ["vape_refill_reallyberry"] = { amount = 50, uses = 8 },
-    ["vape_refill_coconutlimeade"] = { amount = 50, uses = 8 },
-    ["vape_refill_mint"] = { amount = 50, uses = 8 },
-    ["vape_refill_mango"] = { amount = 50, uses = 8 },
-    ["vape_refill_apple"] = { amount = 50, uses = 8 },
-    ["vape_refill_pineapple"] = { amount = 50, uses = 8 },
-    ["vape_refill_lavaflow"] = { amount = 50, uses = 8 },
-    ["vape_refill_allmelon"] = { amount = 50, uses = 8 },
-    ["vape_refill_lemon"] = { amount = 50, uses = 8 },
-    ["vape_refill_peach"] = { amount = 50, uses = 8 }
+    ["vape_refill_strawberry"] = { amount = 50, uses = 5 },
+    ["vape_refill_reallyberry"] = { amount = 50, uses = 5 },
+    ["vape_refill_coconutlimeade"] = { amount = 50, uses = 5 },
+    ["vape_refill_mint"] = { amount = 50, uses = 5 },
+    ["vape_refill_mango"] = { amount = 50, uses = 5 },
+    ["vape_refill_apple"] = { amount = 50, uses = 5 },
+    ["vape_refill_pineapple"] = { amount = 50, uses = 5 },
+    ["vape_refill_lavaflow"] = { amount = 50, uses = 5 },
+    ["vape_refill_allmelon"] = { amount = 50, uses = 5 },
+    ["vape_refill_lemon"] = { amount = 50, uses = 5 },
+    ["vape_refill_peach"] = { amount = 50, uses = 5 }
 }
 
 Config.VapeModel = "prop_vape_01"
