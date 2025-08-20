3bcf27bc905744f5e955e80964628b19c9bd3cdd
shreddykr <148612874+shreddykr@users.noreply.github.com>
2025-08-20 13:33:21 -0700

    config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -5,17 +5,16 @@ Config.MaxHits = 200
 
 Config.RefillItems = {
     ["vape_refill_strawberry"] = { amount = 50, uses = 8 },
-    ["vape_refill_blueberry"] = { amount = 50, uses = 8 },
-    ["vape_refill_grape"] = { amount = 50, uses = 8 },
+    ["vape_refill_reallyberry"] = { amount = 50, uses = 8 },
+    ["vape_refill_coconutlimeade"] = { amount = 50, uses = 8 },
     ["vape_refill_mint"] = { amount = 50, uses = 8 },
     ["vape_refill_mango"] = { amount = 50, uses = 8 },
     ["vape_refill_apple"] = { amount = 50, uses = 8 },
     ["vape_refill_pineapple"] = { amount = 50, uses = 8 },
-    ["vape_refill_bubblegum"] = { amount = 50, uses = 8 },
-    ["vape_refill_cherry"] = { amount = 50, uses = 8 },
-    ["vape_refill_watermelon"] = { amount = 50, uses = 8 },
+    ["vape_refill_lavaflow"] = { amount = 50, uses = 8 },
+    ["vape_refill_allmelon"] = { amount = 50, uses = 8 },
     ["vape_refill_lemon"] = { amount = 50, uses = 8 },
-    ["vape_refill_cola"] = { amount = 50, uses = 8 }
+    ["vape_refill_peach"] = { amount = 50, uses = 8 }
 }
 
 Config.VapeModel = "prop_vape_01"
